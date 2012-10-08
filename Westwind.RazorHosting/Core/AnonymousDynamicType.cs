92d06cee0bfb93ea662ce0cbdeb75c722b8f9077
Rick Strahl <rstrahl@west-wind.com>
2012-10-08 10:33:07 -1000

    Add support for anonymous types
    Add AnonymousDynamic type to handle 'private' objects (like anonymous)
    as models. Dynamic doesn't work against non-public types - anonymous
    properties are internal - so this type provides a dynamic implementation
    using private Reflection to expose the properties. Note: Requires full
    Reflection rights so this might not work in low/medium trust scenarios.

---
+++ Westwind.RazorHosting/Core/AnonymousDynamicType.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Westwind.RazorHosting
+{
+
+    /// <summary>
+    /// This class handles special non-public types - anonymous types
+    /// and allows returning property values from them.
+    /// 
+    /// Requires Reflection permissions for internal,private properties
+    /// </summary>
+    public class AnonymousDynamicType : DynamicObject
+    {
+        /// <summary>
+        /// Internally stored instance on which to look up properties
+        /// via Reflection
+        /// </summary>
+        object Instance = null;
+
+        /// <summary>
+        /// Require passing in off an instance
+        /// </summary>
+        /// <param name="instance"></param>
+        public AnonymousDynamicType(object instance)
+        {
+            Instance = instance;
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            return GetProperty(Instance, binder.Name, out result);
+        }
+
+        private bool GetProperty(object instance, string name, out object result)
+        {
+            if (instance == null)
+                instance = this;
+
+            var miArray = instance.GetType().GetMember(name, BindingFlags.Public | BindingFlags.NonPublic |
+                                                             BindingFlags.GetProperty | BindingFlags.Instance);
+            if (miArray != null && miArray.Length > 0)
+            {
+                var mi = miArray[0];
+                if (mi.MemberType == MemberTypes.Property)
+                {
+                    result = ((PropertyInfo)mi).GetValue(instance, null);
+                    return true;
+                }
+            }
+
+            result = null;
+            return false;
+        }
+    }
+}
