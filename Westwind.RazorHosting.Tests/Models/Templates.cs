715721fd709fc7a24839b02006e32e847696334b
Rick Strahl <rstrahl@west-wind.com>
2014-10-23 12:57:44 -1000

    Add Linq into some of the Samples to ensure LINQ operations are accessible in templates.

--- Westwind.RazorHosting.Tests/Models/Templates.cs
+++ Westwind.RazorHosting.Tests/Models/Templates.cs
@@ -7,8 +7,8 @@ namespace RazorHostingTests
 {
     public class Templates
     {
-       public static string BasicTemplateStringWithPersonModel =
-@"@inherits Westwind.RazorHosting.RazorTemplateBase<RazorHostingTests.Person>
+        public static string BasicTemplateStringWithPersonModel =
+            @"@inherits Westwind.RazorHosting.RazorTemplateBase<RazorHostingTests.Person>
 Hello @Model.Name,
 
 Current time is: @DateTime.Now.
@@ -22,11 +22,16 @@ Assembly: @System.Reflection.Assembly.GetExecutingAssembly().FullName
 <b>@Model.Name of @Model.Company entered on @Model.Entered
 
 Entered on @Model.Entered
+@{ ResultData = ""Hello from the template at "" + DateTime.Now.ToString(""HH:mm""); 
+ // try some LINQ
+ var items = new int[3] { 1,2,3 };
+}
+Linq: @items.Sum() Array items
+Variable Text: @ResultData
 
 Base Type:
 @this.GetType().BaseType.ToString()
 
-@{ ResultData = ""Hello from the template""; }
 ";
     }
 }
